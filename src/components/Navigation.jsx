95c71c0576a904c5c5e94911576dacb7a9c5ad11
Travis Bostick <traviswbostick@gmail.com>
2021-03-17 12:43:05 -0500

    Navbar collapses on select

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,24 +4,47 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 function Navigation() {
+  const [expanded, setExpanded] = useState(false);
   return (
-    <Navbar collapseOnSelect variant='dark' expand='md'>
+    <Navbar
+      expanded={expanded}
+      onToggle={() => setExpanded(!expanded)}
+      variant='dark'
+      expand='md'
+    >
       <Navbar.Brand href='/'>
         <img className='navbar-brand' src='./images/logo.png' alt='logo' />
       </Navbar.Brand>
       <Navbar.Toggle aria-controls='responsive-navbar-nav' />
       <Navbar.Collapse id='responsive-navbar-nav'>
         <Nav className='ml-auto'>
-          <NavLink className='nav-link' exact to='/'>
+          <NavLink
+            className='nav-link'
+            exact
+            to='/'
+            onClick={() => setExpanded(false)}
+          >
             home
           </NavLink>
-          <NavLink className='nav-link' to='/software'>
+          <NavLink
+            className='nav-link'
+            to='/software'
+            onClick={() => setExpanded(false)}
+          >
             software
           </NavLink>
-          <NavLink className='nav-link' to='/sheet-music'>
+          <NavLink
+            className='nav-link'
+            to='/sheet-music'
+            onClick={() => setExpanded(false)}
+          >
             sheet music
           </NavLink>
-          <NavLink className='nav-link' to='/video'>
+          <NavLink
+            className='nav-link'
+            to='/video'
+            onClick={() => setExpanded(false)}
+          >
             video
           </NavLink>
         </Nav>
