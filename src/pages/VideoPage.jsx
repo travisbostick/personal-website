94c9083421e40d0fd36254f78c6923453a28a12b
Travis Bostick <traviswbostick@gmail.com>
2022-01-05 13:42:34 -0600

    Add image load wait to video page

--- src/pages/VideoPage.jsx
+++ src/pages/VideoPage.jsx
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Project from '../components/Project';
 import videos from '../videos';
 import FadeIn from 'react-fade-in';
 
 function VideoPage() {
+  const [imgsLoaded, setImgsLoaded] = useState(false);
+  const IMAGES = videos.map((p, i) => {
+    return { id: i, url: p.cover_img_url };
+  });
+
+  useEffect(() => {
+    const loadImage = image => {
+      return new Promise((resolve, reject) => {
+        const loadImg = new Image();
+        loadImg.src = image.url;
+        loadImg.onload = () => resolve(image.url);
+        loadImg.onerror = err => reject(err);
+      });
+    };
+    Promise.all(IMAGES.map(image => loadImage(image)))
+      .then(() => setImgsLoaded(true))
+      .catch(err => console.log('Failed to load images', err));
+  }, []);
+
   return (
     <div className='software content'>
-      <FadeIn>
-        <div className='row'>
-          {videos.map((project, index) => {
-            project.key = index;
-            return (
-              <Project
-                project={project}
-                image={{ url: project.cover_img_url }}
-              />
-            );
-          })}
-        </div>
-      </FadeIn>
+      {imgsLoaded && (
+        <FadeIn>
+          <div className='row'>
+            {videos.map((project, index) => {
+              project.key = index;
+              return (
+                <Project key={index} project={project} image={IMAGES[index]} />
+              );
+            })}
+          </div>
+        </FadeIn>
+      )}
     </div>
   );
 }
