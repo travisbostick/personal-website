35d8ced758e9f62421fa4826d0af5ddc4b57bc8e
Travis Bostick <traviswbostick@gmail.com>
2022-01-17 23:34:15 -0600

    Update video page to grid

--- src/pages/VideoPage.jsx
+++ src/pages/VideoPage.jsx
@@ -27,7 +27,7 @@ function VideoPage() {
     <div className='software content'>
       {imgsLoaded && (
         <FadeIn>
-          <div className='row'>
+          <div className='grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-y-0 gap-x-5'>
             {videos.map((project, index) => {
               project.key = index;
               return (
